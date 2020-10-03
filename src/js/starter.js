63aaa71ef696264742f28a4edc02606d4338cca0
Aaron Geller <a-geller@northwestern.edu>
2020-10-03 15:52:02 -0500

    added paragraph

--- src/js/starter.js
+++ src/js/starter.js
@@ -1,6 +1,12 @@
 //this file contains any calls that are necessary upon loading the browser window
 
+
 defineParams();
+
+//reset the colors based on the input from params (in case this eventually is read in from a file
+Object.keys(params.boxes).forEach(function(c, i){
+	document.documentElement.style.setProperty('--'+c+'-color',params.boxes[c].color);
+});
 
 // populateBoxes2();
 initBoxes();
